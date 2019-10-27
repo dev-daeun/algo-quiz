8103bdcf18e5b5750c58598db1557e52d3258957
kde6260 <kde6260@gmail.com>
2019-10-27 16:46:47 +0900

    BJ - add test data previous solution used not to pass

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -27,3 +27,22 @@ for i in range(1, N+1):
             values[i][w] = values[i-1][w]
 
 print(values[N][W])
+
+
+# Test 1
+# 5 10
+# 1 1
+# 1 2
+# 1 3
+# 1 4
+# 1 5
+# expected answer: 15
+
+
+# Test 2
+# 4 3
+# 6 13
+# 4 8
+# 3 6
+# 5 12
+# expected answer: 6
