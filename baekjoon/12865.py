8105ee5b121dc68177262252e969069ce7f36b5a
kde6260 <kde6260@gmail.com>
2019-10-27 16:44:09 +0900

    BJ - solve 12865 by renewing optimal value in case that i-th weight is bigger than w

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -9,18 +9,21 @@ class Thing:
         self.weight = weight
 
 
-things = []
+# 1-index based
+things = [Thing(val=0, weight=0)]
 for _ in range(N):
     w, v = list(map(lambda x: int(x), stdin.readline().split()))
     things.append(Thing(val=v, weight=w))
 
 values = []
-for _ in range(N):
+for _ in range(N+1):
     values.append([0 for _ in range(W+1)])
 
-for i in range(1, N):
+for i in range(1, N+1):
     for w in range(1, W+1):
         if things[i].weight <= w:
-            values[i][w] = max(values[i-1][w], values[i-1][W-things[i].weight] + things[i].val)
+            values[i][w] = max(values[i-1][w], values[i-1][w-things[i].weight] + things[i].val)
+        else:
+            values[i][w] = values[i-1][w]
 
-print(values[N-1][W])
+print(values[N][W])
