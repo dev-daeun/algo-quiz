52badb077170b105140c7f33870bf54daef58c4c
kde6260 <kde6260@gmail.com>
2019-11-08 00:57:09 +0900

    BJ - consider edge case in 1107 but does not pass

--- baekjoon/1107.py
+++ baekjoon/1107.py
@@ -18,9 +18,15 @@ def get_answer():
         if b not in broken_buttons:
             able_buttons.append(b)
 
+    if not able_buttons:
+        return answer
+
     for i in range(len(dest_list)):
         min_ = min([abs(dest_list[i] - b) for b in able_buttons])
         channel_to_jump.append(abs(dest_list[i] - min_))
+
+    if channel_to_jump == [0 for _ in range(len(channel_to_jump))]:
+        return answer
 
     channel_to_jump = int(''.join(str(n) for n in channel_to_jump))
     answer = min(answer, abs(dest - channel_to_jump) + len(dest_list))
@@ -54,3 +60,18 @@ print(get_answer())
 # 1
 # output:
 # 0
+
+# test4
+# input:
+# 500000
+# 10
+# 0 1 2 3 4 5 6 7 8 9
+# output:
+# 499900
+
+# test5
+# input:
+# 200
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 100
