5d6366e8907784f87166027fc2f9cc0e735da9c5
kde6260 <kde6260@gmail.com>
2019-11-09 18:36:49 +0900

    BJ - create solution of 10971 but time exceeds

---
+++ baekjoon/10971.py
@@ -0,0 +1,33 @@
+from sys import stdin
+from itertools import permutations
+
+N = int(stdin.readline())
+W = list()
+for _ in range(N):
+    row = list(map(int, stdin.readline().split()))
+    W.append(row)
+
+
+def total_cost_per_path(perm):
+    total_cost = 0
+    for i in range(len(perm)):
+        if i == len(perm) - 1:
+            cost = W[perm[i]][perm[0]]
+        else:
+            cost = W[perm[i]][perm[i+1]]
+        
+        if cost == 0:
+            return None
+        total_cost += cost
+    
+    return total_cost
+
+perms = permutations(range(N))
+nominates = list()
+for p in perms:
+    total_cost = total_cost_per_path(p)
+    if total_cost:
+        nominates.append(total_cost)
+
+answer = min(nominates)
+print(answer)
