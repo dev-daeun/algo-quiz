49b5e173d9c18145c170c8bac2ba7068835a2589
kde6260 <kde6260@gmail.com>
2019-11-12 23:10:16 +0900

    BJ - solve 14889

--- baekjoon/14889.py
+++ baekjoon/14889.py
@@ -7,13 +7,18 @@ for _ in range(N):
     P.append(list(map(int, stdin.readline().split())))
 
 
-def add_power(s, n):    
+def calculate_power(s):
     power = 0
-    for i in range(N):
-        if s & (1 << i):
-            power += P[i][n]
-            power += P[n][i]
-    
+    elements = list()
+
+    for n in range(N):
+        if s & (1 << n):
+            elements.append(n)
+
+    for i in elements:
+        for j in elements:
+            power += P[i][j]
+
     return power
 
 
@@ -23,24 +28,21 @@ def get_answer():
 
     for s1 in range(1 << N):
         s2 = U - s1
-        
+
         s1_cnt = 0
         s2_cnt = 0
-        
-        s1_power = 0
-        s2_power = 0
 
         for n in range(N):
             if s1 & (1 << n):
                 s1_cnt += 1
-                s1_power += add_power(s1, n)
             else:
                 s2_cnt += 1
-                s2_power += add_power(s2, n)
 
         if s1_cnt == s2_cnt == (N / 2):
+            s1_power = calculate_power(s1)
+            s2_power = calculate_power(s2)
             answer = min(answer, abs(s1_power - s2_power))
-    
+
     return answer
 
 
