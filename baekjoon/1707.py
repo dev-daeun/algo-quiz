fd13e0a41e1ada4334f1f8292b9a72d576a90b32
kde6260 <kde6260@gmail.com>
2019-11-18 22:25:18 +0900

    BJ - solve 1707

--- baekjoon/1707.py
+++ baekjoon/1707.py
@@ -4,23 +4,30 @@ S1_VISITED = 0
 S2_VISITED = 1
 NOT_VISITED = 2
 
-def is_binary_graph(V, adjacent_list):
-    status = [NOT_VISITED for _ in range(V)]
-
-    init = 0
+def is_binary_graph(V, i, status, adjacent_list):
     queue = list()
-    queue.append(init)
-    status[init] = S1_VISITED
+    queue.append(i)
+    status[i] = S1_VISITED
 
     while queue:
         x = queue.pop(0)
         for v in adjacent_list[x]:
             if status[v] == status[x]:
-                return 'NO'
+                return False
             if status[v] == NOT_VISITED:
                 status[v] = S1_VISITED if status[x] == S2_VISITED else S2_VISITED
                 queue.append(v)
     
+    return True
+
+
+def search_whole_graph(V, adjacent_list):
+    status = [NOT_VISITED for _ in range(V)]
+    for i in range(V):
+        if status[i] == NOT_VISITED:
+            result = is_binary_graph(V, i, status, adjacent_list)
+            if result == False:
+                return 'NO'
     return 'YES'
 
 
@@ -38,7 +45,7 @@ for _ in range(num_of_test):
         adjacent_list[v1-1].append(v2 - 1)
         adjacent_list[v2-1].append(v1 - 1)
 
-    answers.append(is_binary_graph(V, adjacent_list))
+    answers.append(search_whole_graph(V, adjacent_list))
 
 for ans in answers:
     print(ans)
