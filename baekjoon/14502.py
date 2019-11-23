d7395448759b5a66c279755dff9fb382d1090ebf
kde6260 <kde6260@gmail.com>
2019-11-24 00:18:35 +0900

    BJ - solve 14502 by using combinations & dfs

--- baekjoon/14502.py
+++ baekjoon/14502.py
@@ -1,6 +1,6 @@
 import copy
 
-from itertools import permutations
+from itertools import combinations
 from sys import stdin
 
 
@@ -9,7 +9,20 @@ WALL = '1'
 VIRUS = '2'
 NUM_OF_WALLS = 3
 
+'''
+3 <= ROOM의 갯수 <= 64
 
+최대 64개의 ROOM에서 3개를 선택하여 벽으로 씀. => permutation이 아닌 combination사용.
+(64 * 63 * 62) / (3 * 2 * 1) = 3 ~ 4만
+
+1. 2차원 배열 내 ROOM에 해당하는 칸을 선택하고 WALL으로 값 변경.
+2. 2차원 배열 내 VIRUS에 해당하는 칸을 탐색하면서 인접한 칸 중에 ROOM에 해당하는 칸을 VIRUS로 변경.
+3. VIRUS로 전염된 배열에서 남아있는 ROOM의 수를 세어 최대값을 구한다.
+
+1 ~ 3 과정을 각 조합마다 적용한다.
+
+** is_pushed가 while문 바깥에 정의되어 있어서 무한루프 발생. ** 
+'''
 N, M = list(map(int, stdin.readline().split()))
 pan = list()
 visited = list()
@@ -45,11 +58,10 @@ def dfs(init_x, init_y, copied_pan):
     stack = list()
     stack.append((init_x, init_y))
     visited[init_x][init_y] = True
-    is_pushed = False
 
     while stack:
         x, y = stack[-1]
-        print(f"{x} {y}")
+        is_pushed = False
         for next_x, next_y in adjacent_cells(x, y):
             if 0 <= next_x < N and 0 <= next_y < M:
                 if copied_pan[next_x][next_y] == ROOM and not visited[next_x][next_y]:
@@ -58,7 +70,6 @@ def dfs(init_x, init_y, copied_pan):
                     stack.append((next_x, next_y))
                     is_pushed = True
                     break
-
         if not is_pushed:
             stack.pop(-1)
  
@@ -71,7 +82,7 @@ def get_answer():
             if pan[i][j] == '0':
                 cells_to_be_wall.append(i * M + j)
 
-    three_wall_cases = permutations(cells_to_be_wall, NUM_OF_WALLS)
+    three_wall_cases = combinations(cells_to_be_wall, NUM_OF_WALLS)
     for three_walls in three_wall_cases:
         copied_pan = copy.deepcopy(pan)
         stand_three_walls(three_walls, copied_pan)
@@ -90,6 +101,8 @@ def get_answer():
         answer = max(answer, num_of_rooms)
         restore_visted()
         del copied_pan
+
+    return answer
 
 
 print(get_answer())
