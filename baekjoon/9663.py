81a83591ff959375d814761ea167d5d876e91d71
kde6260 <kde6260@gmail.com>
2019-12-07 20:29:09 +0900

    BJ - replace logic of 9663 with back-tracking but time exceeds

--- baekjoon/9663.py
+++ baekjoon/9663.py
@@ -1,133 +1,41 @@
-from sys import stdin
-
+from sys import stdin, setrecursionlimit
 
 N = int(stdin.readline().strip('\n'))
 
-OCCUPIED = 1
-NOT_OCCUPIED = 0
 
-chess = list()
-for _ in range(N):
-    chess.append([NOT_OCCUPIED for _ in range(N)])
+'''
+loc_of_queens[i] = i번째 열에 있는 퀸이 위치한 행
+'''
+
+answer = 0
+loc_of_queens = [0 for _ in range(N)]
 
 
-def restore_chess():
+def is_promising(prev_col, cur_row):
+    for col in range(prev_col + 1):
+        if loc_of_queens[col] == cur_row or abs(loc_of_queens[col] - cur_row) == abs(col - (prev_col + 1)):
+            return False
+    return True
+
+
+def n_queen(prev_col):
+    global answer
+    if prev_col == N - 1:
+        answer += 1
+        return
+
     for i in range(N):
-        for j in range(N):
-            chess[i][j] = NOT_OCCUPIED
-
-
-def is_conflict(x, y):
-    # 가로 검사
-    for i in range(0, N):
-        if chess[x][i] == OCCUPIED:
-            return True
-
-    # 세로 검사
-    for j in range(0, N):
-        if chess[j][y] == OCCUPIED:
-            return True
-
-    # 왼쪽 대각선 검사
-    for k in range(1, N):
-        if x - k >= 0 and y - k >= 0:
-            if chess[x-k][y-k] == OCCUPIED:
-                return True
-        if x + k < N and y + k < N:
-            if chess[x+k][y+k] == OCCUPIED:
-                return True
-
-    # 오른쪽 대각선 검사
-    for k in range(1, N):
-        if x - k >= 0 and y + k < N:
-            if chess[x-k][y+k] == OCCUPIED:
-                return True
-
-        if x + k < N and y - k >= 0:
-            if chess[x+k][y-k] == OCCUPIED:
-                return True
-
-    return False
-
-
-def occupy(x, y):
-    # 가로 변경
-    for i in range(0, N):
-        chess[x][i] = OCCUPIED
-
-    # 세로 변경
-    for j in range(0, N):
-        chess[j][y] = OCCUPIED
-
-    # 왼쪽 대각선 변경
-    for k in range(1, N):
-        if x - k >= 0 and y - k >= 0:
-            chess[x-k][y-k] = OCCUPIED
-        if x + k < N and y + k < N:
-            chess[x+k][y+k] = OCCUPIED
-
-    # 오른쪽 대각선 변경
-    for k in range(1, N):
-        if x - k >= 0 and y + k < N:
-            chess[x-k][y+k] = OCCUPIED
-
-        if x + k < N and y - k >= 0:
-            chess[x+k][y-k] = OCCUPIED
-
-    chess[x][y] = OCCUPIED
-
-
-def adjacent_list(x, y):
-    return (
-        (x - 2, y + 1),
-        (x + 2, y - 1),
-        (x - 1, y + 2),
-        (x + 1, y - 2),
-        (x + 1, y + 2),
-        (x - 1, y - 2),
-        (x + 2, y + 1),
-        (x - 2, y - 1),
-    )
-
-
-def is_satisfied(init_x, init_y):
-    stack = list()
-    stack.append((init_x, init_y))
-    occupy(init_x, init_y)
-    left_queens = N - 1
-
-    while stack:
-        if not left_queens:
-            return True
-
-        cur_x, cur_y = stack[-1]
-        is_pushed = False
-
-        for next_x, next_y in adjacent_list(cur_x, cur_y):
-            if 0 <= next_x < N and 0 <= next_y < N:
-                if chess[next_x][next_y] == NOT_OCCUPIED:
-                    if not is_conflict(next_x, next_y):
-                        print('called')
-                        occupy(next_x, next_y)
-                        stack.append((next_x, next_y))
-                        is_pushed = True
-                        left_queens -= 1
-                        break
-
-        if not is_pushed:
-            stack.pop(-1)
-
-    return False
+        if is_promising(prev_col, i):
+            loc_of_queens[prev_col+1] = i
+            n_queen(prev_col+1)
 
 
 def get_answer():
-    answer = 0
-
-    for i in range(N):
-        for j in range(N):
-            if is_satisfied(i, j):
-                answer += 1
-            restore_chess()
+    global loc_of_queens
+    for x in range(N):
+        loc_of_queens[0] = x
+        n_queen(0)
+        loc_of_queens = [0 for _ in range(N)]
 
     print(answer)
 
