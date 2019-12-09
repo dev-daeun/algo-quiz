52959cd50682ef2ee82689c2854ea77b20dfb493
kde6260 <kde6260@gmail.com>
2019-12-10 00:19:55 +0900

    BJ - solve 1987 by using ord() in visited alphabets

--- baekjoon/1987.py
+++ baekjoon/1987.py
@@ -7,9 +7,17 @@ cell = list()
 for _ in range(R):
     cell.append(list(stdin.readline().strip('\n')))
 
+'''
+푸는 데 걸린 시간: 3시간
 
-alphabets = [chr(i) for i in range(65, 91)]
-visited = {alpha: False for alpha in alphabets}
+* 푸는 데 시간이 오래 걸린 원인 *
+    1. max() 내장함수에 재귀호출되는 함수를 넘겨줘서 의도한대로 동작하지 않음.
+
+* 시간 초과 원인 *
+    1. visited 컬렉션을 (알파벳 문자: False) 형태의 해시테이블로 만들었음.
+    2. (ASCII 코드 - 65) 인덱스로 접근하는 리스트로 바꾼 후 통과됨.
+'''
+visited = [False for _ in range(26)]
 
 
 def adjacent_list(x, y):
@@ -28,15 +36,17 @@ def get_answer(x, y, cnt):
     global answer
     for next_x, next_y in adjacent_list(x, y):
         if 0 <= next_x < R and 0 <= next_y < C:
-            if not visited[cell[next_x][next_y]]:
-                visited[cell[next_x][next_y]] = True
+            if not visited[ord(cell[next_x][next_y]) - 65]:
+                visited[ord(cell[next_x][next_y]) - 65] = True
+                # 재귀호출하는 함수를 넣어서 그런지
+                # max(answer, get_answer(next_x, next_y, cnt + 1)) 으로 호출하면 의도하는 값이 안나옴.
                 result = get_answer(next_x, next_y, cnt + 1)
                 if result > answer:
                     answer = result
-                visited[cell[next_x][next_y]] = False
+                visited[ord(cell[next_x][next_y]) - 65] = False
     return cnt
 
 
-visited[cell[0][0]] = True
+visited[ord(cell[0][0]) - 65] = True
 get_answer(0, 0, 1)
 print(answer)
