ce0a2b4f594b1e0b1c02498fe654387e944194a0
kde6260 <kde6260@gmail.com>
2019-12-10 22:51:20 +0900

    BJ - create solution of 2580 but did not pass

---
+++ baekjoon/2580.py
@@ -0,0 +1,48 @@
+from sys import stdin
+
+cells = list()
+for _ in range(9):
+    cells.append(list(map(int, stdin.readline().strip('\n').split())))
+
+
+empty_cells = list()
+for i in range(9):
+     for j in range(9):
+        if cells[i][j] == 0:
+            empty_cells.append((i, j))
+
+
+def garo(x, y):
+    return [cells[x][j] for j in range(9)]
+
+
+def sero(x, y):
+    return [cells[i][y] for i in range(9)]
+
+
+def sagak(x, y):
+    result = list()
+    for i in range(3 *(x//3), 3*(x//3) + 3):
+        for j in range(3*(y//3), 3*(y//3) + 3):
+            if i != x or j != y:
+                result.append(cells[i][j])
+    return result
+
+
+def answer(idx, result):
+    if len(result) == len(empty_cells):
+        for r, corp in zip(result, empty_cells):
+            r = int(r)
+            x, y = corp
+            cells[x][y] = r
+        return
+
+    x, y = empty_cells[idx]
+    for i in range(1, 10):
+        if (i not in garo(x, y)) and (i not in sero(x, y)) and (i not in sagak(x, y)):
+            answer(idx + 1, result + str(i))
+
+answer(0, '')
+for row in cells:
+    print(' '.join(str(e) for e in row))
+
