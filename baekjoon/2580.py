fae802565685246304ab1f7eb54a97bb06d825f5
kde6260 <kde6260@gmail.com>
2019-12-11 18:31:41 +0900

    BJ - add explanation on 2580

--- baekjoon/2580.py
+++ baekjoon/2580.py
@@ -40,6 +40,10 @@ def sagak(x, y, i):
 
 def answer(idx):
     if idx == len(empty_cells):
+        '''
+        스도쿠 판이 모두 채워졌을 경우 판을 모두 출력한 후 프로세스를 종료한다.
+        프로세스 종료가 아닌 리턴을 할 경우 판이 모두 채워진 뒤에 아직 종료되지 않은 재귀가 실행됨.
+        '''
         for i in range(9):
             for j in range(9):
                 print(cells[i][j], end=' ')
@@ -51,6 +55,11 @@ def answer(idx):
         if garo(x, i) and sero(y, i) and sagak(x, y, i):
             cells[x][y] = i
             answer(idx + 1)
+            '''
+            재귀호출을 한 뒤에 cell을 다시 0으로 갱신한다.
+            cells는 스택에 쌓인 모든 함수들이 참조하는 전역변수이므로 
+            어떤 재귀함수에 의해 변경된 cell의 값이 다른 재귀함수에서 숫자를 채우기 위한 조건을 검사하는 데 영향을 준다.
+            '''
             cells[x][y] = 0
 
 
