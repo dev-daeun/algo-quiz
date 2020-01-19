6e0777e2f531551ea10991c7f14d88d1cda916f3
kde6260 <kde6260@gmail.com>
2020-01-19 17:13:38 +0900

    remove Vertex class & use heappush, heappop but still does not pass

--- baekjoon/1753.py
+++ baekjoon/1753.py
@@ -1,56 +1,44 @@
-from heapq import heapify, heappop
-from sys import stdin
+from heapq import heappop, heappush, heapify
+from sys import stdin, maxsize
 
 
-class Vertex:
-    def __init__(self, number):
-        self.number = number
-        self.dist = 11
-
-    def __lt__(self, other):
-        return self.dist < other.dist
-
-
-def relax(v, adj_v, w, vertices):
-    if vertices[v].dist + w < vertices[adj_v].dist:
-        vertices[adj_v].dist = vertices[v].dist + w
+def relax(v, adj_v, w, dist):
+    if dist[adj_v] > dist[v] + w:
+        dist[adj_v] = dist[v] + w
+        return True
+    return False
 
 
 def dijkstra(start, adjacent_list, n):
-    vertices = [Vertex(i) for i in range(n)]
-    vertices[start].dist = 0
+    dist = [INF for _ in range(n)]
+    dist[start] = 0
 
     q = list()
-    for i in range(n):
-        q.append(vertices[i])
+    heapify(q)
+    heappush(q, (dist[start], start))
 
     while q:
-        heapify(q)
-        v = heappop(q)
+        dist_v, v = heappop(q)
+        for adj_v, adj_w in adjacent_list[v]:
+            is_relaxed = relax(v, adj_v, adj_w, dist)
+            if is_relaxed:
+                heappush(q, (dist[adj_v], adj_v))
 
-        for adj_v, adj_w in adjacent_list[v.number]:
-            relax(v.number, adj_v, adj_w, vertices)
-
-    return vertices
+    return dist
 
 
-if __name__ == '__main__':
-    n, m = list(map(int, stdin.readline().split()))
-    start = int(stdin.readline().strip('\n'))
-    start -= 1  # 0-index based
+INF = 11
 
-    adjacent_list = [list() for _ in range(n)]
-    for _ in range(m):
-        v1, v2, w = list(map(int, stdin.readline().split()))
-        if len(adjacent_list[v1-1]) > 0:
-            for adj in adjacent_list[v1-1]:
-                adj_v, adj_w = adj
-                if adj_v == v2 - 1 and adj_w > w:
-                    adjacent_list[v1-1].pop(adjacent_list[v1-1].index(adj))
+n, m = list(map(int, stdin.readline().split()))
+start = int(stdin.readline().strip('\n'))
+start -= 1  # 0-index based
 
-        adjacent_list[v1-1].append((v2 - 1, w))
+adjacent_list = [list() for _ in range(n)]
+for _ in range(m):
+    v1, v2, w = list(map(int, stdin.readline().split()))
+    adjacent_list[v1-1].append((v2 - 1, w))
 
-    vertices = dijkstra(start, adjacent_list, n)
+result = dijkstra(start, adjacent_list, n)
 
-    for v in vertices:
-        print('INF') if v.dist == 11 else print(v.dist)
+for i in range(n):
+    print('INF') if result[i] == INF else print(result[i])
